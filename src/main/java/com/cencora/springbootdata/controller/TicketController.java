383ba3564dbf199b7d65442fd78be73ab666d3ee
deshmukh.prasad.v <deshmukh.prasad.v@gmail.com>
2025-12-11 11:30:06 +0530

    1234

--- src/main/java/com/cencora/springbootdata/controller/TicketController.java
+++ src/main/java/com/cencora/springbootdata/controller/TicketController.java
@@ -3,6 +3,7 @@ package com.cencora.springbootdata.controller;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 import com.cencora.springbootdata.model.Ticket;
 import com.cencora.springbootdata.repository.TicketRepository;
@@ -18,5 +19,10 @@ public class TicketController {
 	@GetMapping("/getAll")
 	public Iterable<Ticket> getAllTickets(){
 		return ticketRepository.findAll();
+	}
+	
+	@GetMapping("get/{id}")
+	public Ticket getTicketById(@PathVariable("id") int index) {
+		return ticketRepository.findById(index).get();
 	}
 }
